edfd8eba3f6f94612999e91914063b2c1194640c
akashbhagat354 <akashbhagat132@gmail.com>
2021-05-29 18:48:55 +0530

    edited by akash

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import Dashboard from './Components/Dashboard';
+import Registration from './Components/Registration';
+import Profile from './Components/Profile';
+import Tables from './Components/Tables';
+import Wizard from './Components/Wizard';
+import ProductCart from './Components/ProductCart';
+import Check from './Components/Check';
+
+
+function App() {
+  return (
+    <div>
+      {/* <Dashboard></Dashboard> */}
+      {/* <Registration></Registration> */}
+      {/* <Profile></Profile> */}
+      {/* <Tables></Tables> */}
+      {/* <Wizard></Wizard> */}
+      {/* <ProductCart></ProductCart> */}
+      {/* <Check></Check> */}
+    </div>
+   
+  )
+}
+
+export default App;
